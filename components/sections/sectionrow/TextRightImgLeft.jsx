cd370200d11ec3616db911e68f230b118bbd217d
xxrobone <robert.wagar@edu.cmeducations.se>
2023-08-18 14:43:33 +0200

    border box

--- components/sections/sectionrow/TextRightImgLeft.jsx
+++ components/sections/sectionrow/TextRightImgLeft.jsx
@@ -5,10 +5,10 @@ import IMG from '@/public/piano.jpg';
 const TextRightImgLeft = ({ children }) => {
   return (
     <div className='h-full w-full flex flex-col-reverse md:flex-row'>
-      <div className='w-full h-full relative z-1 grid place-content-center p-20 md:p-20 lg:p-40'>
+      <div className='w-full h-full relative z-1 grid place-content-center p-20 md:p-20 lg:p-40 border-box'>
         {children}
       </div>
-      <div className='w-full h-full relative z-1 grid place-content-center xs:order-1'>
+      <div className='w-full h-full relative z-1 grid place-content-center xs:order-1 border-box'>
         <Image
           className='object-cover'
           quality={100}
