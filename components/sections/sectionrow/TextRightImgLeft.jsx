5516097d7b1dfb337be99bef17560141f2701df8
xxrobone <robert.wagar@edu.cmeducations.se>
2023-08-18 14:34:34 +0200

    fixes on singleitem

--- components/sections/sectionrow/TextRightImgLeft.jsx
+++ components/sections/sectionrow/TextRightImgLeft.jsx
@@ -5,10 +5,10 @@ import IMG from '@/public/piano.jpg';
 const TextRightImgLeft = ({ children }) => {
   return (
     <div className='h-full w-full flex flex-col-reverse md:flex-row'>
-      <div className='w-1/2 h-full relative z-1 grid place-content-center p-20 md:p-20 lg:p-40 xs:order-2'>
+      <div className='w-full h-full relative z-1 grid place-content-center p-20 md:p-20 lg:p-40'>
         {children}
       </div>
-      <div className='w-1/2 h-full relative z-1 grid place-content-center xs:order-1 cursor-pointer transition-none 300ms ease-in-out'>
+      <div className='w-full h-full relative z-1 grid place-content-center xs:order-1 cursor-pointer transition-none 300ms ease-in-out'>
         <Image
           className='object-cover'
           quality={100}
