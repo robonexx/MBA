94bbf5241e01575d27c6e1fd16a72161de335cf1
xxrobone <robert.wagar@edu.cmeducations.se>
2023-08-18 12:07:06 +0200

    sectionrow wrapper

--- components/sections/sectionrow/SectionRow.tsx
+++ components/sections/sectionrow/SectionRow.tsx
@@ -6,7 +6,7 @@ type PropsWithChildren = {
 
 const SectionRow = ({ children }: PropsWithChildren) => {
   return (
-    <div className='w-screen h-screen block bg-fuchsia-950 m-0 p-0' >
+    <div className='w-screen h-screen block bg-indigo-950 m-0 p-0' >
       {children}
     </div>
   );
