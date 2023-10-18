9c2bfeb8249b58cdd0bd318e8de67c7a373f2bae
robonexx <robertwagar@gmail.com>
2023-10-18 19:25:31 +0200

    film scroll with js

--- app/(site)/film/page.tsx
+++ app/(site)/film/page.tsx
@@ -1,20 +1,54 @@
+'use client';
+
+import React, { useEffect, useRef } from 'react';
 import FilmItem from '@/components/singleitem/FilmItem';
 import { getFilms } from '@/sanity/sanity-utils';
 import { FilmTypes } from '@/sanity/types/FilmType';
-import React from 'react';
+import styles from './film.module.scss';
 
-import styles from './film.module.scss'
+const FilmPage: React.FC = () => {
+  const [films, setFilms] = React.useState<FilmTypes[]>([]);
 
-export const revalidate = 20;
-export const fetchCache = 'force-no-store';
-export const dynamic = 'force-dynamic';
+  const filmRef = useRef<HTMLDivElement>(null);
 
-const page = async () => {
-  const films: FilmTypes[] = await getFilms();
+  useEffect(() => {
+    const fetchData = async () => {
+      const filmsData: FilmTypes[] = await getFilms();
+      setFilms(filmsData);
+    };
 
-  console.log(films);
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const parentContainer = filmRef.current?.addEventListener; // Access current property
+
+    if (parentContainer) {
+      parentContainer('scroll', handleScroll);
+
+      return () => {
+        const currentRef = filmRef.current; // Capture the current value
+        currentRef?.removeEventListener('scroll', handleScroll);
+      };
+    }
+  }, []); // Add filmRef.current to dependencies
+
+  const handleScroll = (event: Event) => {
+    const scrollPos = (event.currentTarget as HTMLDivElement).scrollTop;
+    const childHeight = (event.currentTarget as HTMLDivElement).clientHeight;
+
+    // Calculate the index of the currently visible item
+    const currentIndex = Math.round(scrollPos / childHeight);
+
+    // Smoothly scroll to the next item
+    (event.currentTarget as HTMLDivElement).scrollTo({
+      top: currentIndex * childHeight,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <div className={styles.film}>
+    <div className={styles.film} ref={filmRef}>
       {films &&
         films.map((data) => (
           <div key={data._id} className={styles.item}>
@@ -25,4 +59,4 @@ const page = async () => {
   );
 };
 
-export default page;
+export default FilmPage;
